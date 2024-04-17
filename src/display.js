2afa68f98aad960eb4cbf91f073120c8d62d98e3
nebiat <nebiatelias@outlook.com>
2024-04-16 22:55:52 -0700

    changed tasks to forms. need to fix width

--- src/display.js
+++ src/display.js
@@ -1,5 +1,4 @@
 // TODO:
-// - change task items to be forms
 // - set up listeners for the buttons and input
 //      - edit/delete/display notes
 //      - add dialogs and modals for adding new tasks and groups
