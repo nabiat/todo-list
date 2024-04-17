7bad82012af790d98ed2707a5557e859a9276f29
nebiat <nebiatelias@outlook.com>
2024-04-16 22:55:11 -0700

    changed tasks to forms. need to fix width

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './style.css';
+
+// FOR TEMPLATE HTML
+
+// Delete tasks listeners
+let deleteBtns = document.querySelectorAll('.delete');
+deleteBtns.forEach((btn) =>
+    btn.addEventListener('click', () => {
+        btn.parentNode.parentNode.remove();
+    })
+);
+
+// Edit tasks listeners
+let editBtns = document.querySelectorAll('.edit');
+editBtns.forEach((btn) =>
+    btn.addEventListener('click', () => {
+        let date = btn.parentNode.querySelector('.task-due');
+        let name = btn.parentNode.parentNode.querySelector('.task-name');
+    
+        date.textContent = 'new date';
+        name.textContent = 'new name';
+    })
+);
+
+let groupDeleteBtn = document.querySelector('.delete-group');
+groupDeleteBtn.addEventListener('click', () => {
+    groupDeleteBtn.parentNode.remove();
+    document.querySelector('.right#default').remove();
+});
+
+let editTitleBtn = document.querySelector('.edit-title');
+editTitleBtn.addEventListener('click', () => {
+    editTitleBtn.parentNode.querySelector('.group_name').textContent = 'new title';
+});
